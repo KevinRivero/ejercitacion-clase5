dfb414bd3d34fc101da97f678c6a7c020a55d1f1
KevinRivero <kevin_xavier_rivero@hotmail.com>
2022-05-28 14:08:59 -0300

    segunda parte de receta asado

--- asado.js
+++ asado.js
@@ -1,1 +1,10 @@
 asado al horno  y a la parrilla
+2 kg chinchulín
+salchicha parrillera
+1 pollo de campo
+Sal parrillera (mi abuela le ponía sal gruesa)
+limón para el pollo
+Optativo: Chimichurri
+
+
+
