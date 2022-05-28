f9e263d9ea408402a49a0115b804b5210af6fff8
Yesica <yzdaanalytics@gmail.com>
2022-05-28 14:10:40 -0300

    receta delasado

--- asado.js
+++ asado.js
@@ -1,1 +1,5 @@
 asado al horno  y a la parrilla
+2 kg carne (un poco de asado de tira, vacío, tapa de asado, entraña, colita de cuadril, matambre, falda parrillera o si tenés algún corte que te guste lo incluís)
+4 morcillas bombón (si es de las grandes la fraccionás cuando servís)
+4 chorizos de cerdo
+2 riñones
